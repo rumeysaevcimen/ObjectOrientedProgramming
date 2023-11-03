92e25276b7a52a6b5f2783acb49c785ad09fd7c6
Rümeysa Evcimen <evcimenr@gmail.com>
2023-11-03 09:51:07 +0300

    Delete circle directory

--- circle/src/CircleTest.java
+++
@@ -1,7 +0,0 @@
-public class CircleTest {
-    public static void main(String[] args) {
-        Circle c1 = new Circle(5.0);
-        c1.setYaricap(-2);
-        System.out.println(c1.alanHesapla());
-    }
-}
